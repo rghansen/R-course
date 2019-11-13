b915940f48ee83f704a244d727a23f07c5f2b601
Rebecca Hansen <rebeccagunnhansen@gmail.com>
2019-11-12 22:36:04 -0800

    Arranging and labelling functions

--- Section 4 script.R
+++ Section 4 script.R
@@ -1,5 +1,16 @@
+#Arithmetic conditional functions
 x <- 0.001
 if(x > 0){print("positive")} else {print("not positive")}
-
 x <- -0.001
 if(x > 0){print("positive")} else {print("not positive")}
+x <- 1
+if(x >= 0) {print(log(x))} else {print("error")}
+x <- -1
+if(x >= 0) {print(log(x))} else {print("error")}
+
+#Situational conditional function
+library(dslabs)
+data(murders)
+murder_rate <-murders$total/murders$population*100000
+ind <- which.min(murder_rate)
+if(murder_rate[ind] < 0.5) {print(murders$state[ind])} else {print("No state has murder rate that low")}
