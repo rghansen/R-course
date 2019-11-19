ee37d2766fdb859f3d01f282809bf539a7ac4afc
Rebecca Hansen <rebeccagunnhansen@gmail.com>
2019-11-18 17:06:49 -0800

    Starting new R course with swirl() tutorial

---
+++ Data and Stats Course/Getting started and assessment 1.R
@@ -0,0 +1,18 @@
+#package for course
+install.packages("rafalib")
+library("rafalib")
+
+#built-in r tutorial
+install.packages("swirl")
+library("swirl")
+swirl()
+
+#how to work in swirl: esc button to prompt
+#actions from the prompt: When you are at the R prompt (>):
+# -- Typing skip() allows you to skip the current question.
+# -- Typing play() lets you experiment with R on your own; swirl will ignore what you do...
+# -- UNTIL you type nxt() which will regain swirl's attention.
+# -- Typing bye() causes swirl to exit. Your progress will be saved.
+# -- Typing main() returns you to swirl's main menu.
+# -- Typing info() displays these options again
+
