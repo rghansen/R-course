e5d246b87b7dbeebcaa37d78d3ead42eaa82ca85
Rebecca Hansen <rebeccagunnhansen@gmail.com>
2019-11-19 15:13:21 -0800

    First assessment tasks

--- Data and Stats Course/Getting started and assessment 1.R
+++ Data and Stats Course/Getting started and assessment 1.R
@@ -16,3 +16,26 @@ swirl()
 # -- Typing main() returns you to swirl's main menu.
 # -- Typing info() displays these options again
 
+#assessment for getting started section
+version
+firstvector <- c(2.23,3.45,1.87,2.11,7.33,18.34,19.23)
+mean(firstvector)
+
+#calculating sum of i^2 between 1 and 25
+firstloop <- vector("numeric",length=25)
+for(i in 1:25) {
+  firstloop[i] <- i^2
+}
+sum(firstloop)
+
+#working with cars database
+class(cars)
+nrow(cars)
+head(cars)
+
+#calculating mean distance for whole
+mean(cars[,2])
+
+#finding a specific entry 
+which(cars$dist == 85)
+cars$dist[50]
