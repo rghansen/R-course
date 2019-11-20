0861aacd09367d3a4b8ab2640f59f161bac1de34
Rebecca Hansen <rebeccagunnhansen@gmail.com>
2019-11-19 20:38:41 -0800

    Annotating

--- Statistics and Data Visualization/Data setup.R
+++ Statistics and Data Visualization/Data setup.R
@@ -1,3 +1,5 @@
+#reading in data for this section
 femweights <- read.csv("Statistics and Data Visualization/femaleMiceWeights.csv")
 femcontrol <- read.csv("Statistics and Data Visualization/femaleControlsPopulation.csv")
 pheno <- read.csv("Statistics and Data Visualization/mice_pheno.csv")
+
