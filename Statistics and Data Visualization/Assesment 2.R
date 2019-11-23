b2360b64a70b41027e2ed8ed2a95a63dd582d70d
Rebecca Hansen <rebeccagunnhansen@gmail.com>
2019-11-22 22:13:39 -0800

    Thought I had already saved this

--- Statistics and Data Visualization/Assesment 2.R
+++ Statistics and Data Visualization/Assesment 2.R
@@ -1,1 +1,25 @@
-read.csv("Statistics and Data Visualization/msleep_ggplot2.csv")
+#reading in data (data already downloased from github)
+msleep <- read.csv("Statistics and Data Visualization/msleep_ggplot2.csv")
+class(msleep)
+structure(msleep)
+
+#selecting only primates
+primates <- filter(msleep, order == "Primates")
+nrow(primates)
+class(primates)
+
+#selecting sleep of primates
+select(primates, sleep_total) %>% class()
+
+#transforming sleep into a vector to calculate the mean
+primatesleep <- select(primates, sleep_total) %>% unlist()
+mean(primatesleep)
+
+#learning and using the summarize function
+?summarize
+
+#using summarize with group_by
+msleep %>% group_by(order) %>% summarize(mean(sleep_total))
+
+#using summarize with filter
+msleep %>% filter(order == "Primates") %>% summarize(mean(sleep_total))
