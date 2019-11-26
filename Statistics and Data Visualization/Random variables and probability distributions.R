96210cd8ada9bc52b609d6ecaad2497617fdc557
Rebecca Hansen <rebeccagunnhansen@gmail.com>
2019-11-26 15:53:11 -0800

    Creating a null distribution using a for loop of repeat sampling

--- Statistics and Data Visualization/Random variables and probability distributions.R
+++ Statistics and Data Visualization/Random variables and probability distributions.R
@@ -14,3 +14,23 @@ class(populationweights)
 #sampling from the population (repeat as many times as you want to )
 mean(sample(populationweights, 12))
 
+#difference between control and treatment means
+difference <- abs(mean(control) - mean(treatment))
+
+#null hypothesis: there is no difference between the mean weights of the control and
+#treatment mice therefore they represent samples from the same population
+controln <- sample (populationweights, 12)
+treatmentn <- sample (populationweights, 12)
+differencen <- (mean(controln)- mean(treatmentn))
+
+#repeat random sample a bunch of times to generate a null distribution using for loop
+n <- 10000
+distofdiff <- vector(length = 10000, "numeric")
+for (i in 1:n) {
+  x <- sample(populationweights, 12)
+  y <- sample(populationweights, 12)
+  distofdiff[i] <- mean(x) - mean(y)
+}
+
+#visualize null distribution as histogram
+hist(distofdiff)
