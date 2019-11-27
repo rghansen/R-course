ac41062430049b54bf9a4f041a8f7bf5c529a845
Rebecca Hansen <rebeccagunnhansen@gmail.com>
2019-11-26 16:51:05 -0800

    Assessment of creating distributions and calculating p-values

--- Statistics and Data Visualization/Assesment for random variables and probability distributions.R
+++ Statistics and Data Visualization/Assesment for random variables and probability distributions.R
@@ -14,3 +14,33 @@ abs(mx-mfiveofx)
 set.seed(5)
 mfiveofx1 <- mean(sample(x,5))
 abs(mx-mfiveofx1)
+
+#creating a null distribution based on 1000 samples
+set.seed(1)
+n <- 1000
+popmeans <- vector(length = 1000, "numeric")
+for(i in 1:n) {
+  popmeans[i] <- mean(sample(x,5))
+}
+
+#calculating a proportion for being within +/- 1 grams of the population mean
+sum(popmeans > 1 + mean(x) | popmeans < mean(x) - 1)/1000
+
+#creating a null distribution based on 10000 samples and calculating the same proportion
+set.seed(1)
+n <- 10000
+popmeans <- vector(length = 10000, "numeric")
+for(i in 1:n) {
+  popmeans[i] <- mean(sample(x,5))
+}
+sum(popmeans > 1 + mean(x) | popmeans < mean(x) - 1)/10000
+
+#creating a null distribution based on 1000 samples, sample size 50, then proportion
+set.seed(1)
+n <- 1000
+popmeans <- vector(length = 1000, "numeric")
+for(i in 1:n) {
+  popmeans[i] <- mean(sample(x,50))
+}
+sum(popmeans > 1 + mean(x) | popmeans < mean(x) - 1)/1000
+
